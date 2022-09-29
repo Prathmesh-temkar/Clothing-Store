62b38110992f12c5cff1759c8d342b0191d8dee6
unknown <prathmesh.temkar@gmail.com>
2022-09-29 11:59:42 +0530

    Navigation Bar Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 
+const Shop = () => {
+  return <h1>I am Shop Page</h1>;
+};
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
